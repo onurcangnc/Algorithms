27da0808601aba85cbdc186aa72401cabbcb34fe
Onurcan Genç <onurcan.genc@ug.bilkent.edu.tr>
2024-04-08 20:21:14 +0300

    BubbleSort optimization.

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,7 +1,18 @@
 import numpy as np
+import time as t
 
+# Bubble Sort
+# Time Complexity: O(n^2)
+
+start_time = t.time()
 arr = np.array([56, 12, 78, 39, 23, 88, 45, 10, 91, 5])
 size = len(arr)
+
+def is_sorted(arr, size):
+    for i in range(size-1):
+        if arr[i] > arr[i+1]:
+            return False
+    return True
 
 def bubbleSort(arr, size):
     for i in range(size-1):
@@ -11,4 +22,9 @@ def bubbleSort(arr, size):
     return arr
 
 print("Original Array: ", arr)
-print("Sorted Array: ", bubbleSort(arr, size))
+if(is_sorted(arr, size)):
+    print("Array is already sorted")
+else:
+    print("Sorted Array: ", bubbleSort(arr, size))
+end_time = t.time()
+print("Time taken: ", end_time-start_time, "seconds")
